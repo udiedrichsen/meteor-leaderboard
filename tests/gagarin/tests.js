2e2ace2e37e7abc8d94fecc3e1fd97b19acd9778
Ulrich Diedrichsen <ulrich.diedrichsen@googlemail.com>
2015-12-02 10:42:04 +0100

    .

---
+++ tests/gagarin/tests.js
@@ -0,0 +1,9 @@
+describe('My first Gagarin test suite', function () {
+  var server = meteor();
+  it('should just work', function () {
+    return server.execute(function () { 
+      console.log('I am alive!'); 
+    });
+  });
+});
+
